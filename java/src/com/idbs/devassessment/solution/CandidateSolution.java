67621dc5a9eb3a570fbafd139b9e76f76f356e1b
Winndie Fan <winndie@gmail.com>
2021-02-25 18:35:43 +0000

    Test ok: level 1&2

--- java/src/com/idbs/devassessment/solution/CandidateSolution.java
+++ java/src/com/idbs/devassessment/solution/CandidateSolution.java
@@ -4,72 +4,149 @@
  */
 package com.idbs.devassessment.solution;
 
-import java.io.StringReader;
+import com.idbs.devassessment.core.DifficultyLevel;
+import com.idbs.devassessment.core.IDBSSolutionException;
 
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
-
-import com.idbs.devassessment.core.IDBSSolutionException;
-import com.idbs.devassessment.core.DifficultyLevel;
+import java.io.StringReader;
 
 /**
  * Example solution for the example question
  */
 
-public class CandidateSolution extends CandidateSolutionBase
-{
+public class CandidateSolution extends CandidateSolutionBase {
     @Override
-    public DifficultyLevel getDifficultyLevel()
-    {
-        /*
-         * 
-         * CHANGE this return type to YOUR selected choice of difficulty level to which you will code an answer to.
-         * 
-         */
-
-        return DifficultyLevel.LEVEL_1;
+    public DifficultyLevel getDifficultyLevel() {
+        return DifficultyLevel.LEVEL_2;
     }
 
-    @Override
-    public String getAnswer() throws IDBSSolutionException
-    {
-        /*
-         * This is the default solution and provides some example code on how to extract data from Json in java.
-         *
-         * As an initial start we suggest you comment ALL the code below and return a null value from the method. Run
-         * this in the assessment application and you'll see many examples of the Json that question produces.
-         */
+    private long getPower(int power, long baseValue) {
 
-        // first get Json as a String for the question using the inherited method...
-        String json = getDataForQuestion();
+        long answer = 0;
 
-        // now use the json api to read the json to give a JsonObject representing the Json...
+        if (power == 0) {
+
+            answer = 1;
+
+        } else if (power == 1) {
+
+            answer = baseValue;
+
+        } else if (power > 1) {
+
+            answer = baseValue;
+
+            for (int i = 1; i < power; i++) {
+
+                long powerVal = 0;
+
+                for (int j = 1; j <= baseValue; j++) {
+                    if (j == 1) {
+                        powerVal = 0;
+                    } else if (j == 2) {
+                        powerVal = answer;
+                    }
+                    answer = answer + powerVal;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    private long getMultiple(int multiplier, long baseValue) {
+
+        long answer = 0;
+
+        for (int i = 1; i <= multiplier; i++) {
+            answer = answer + baseValue;
+        }
+
+        return answer;
+    }
+
+    private long getAddSub(boolean isAdd, long a, long b) {
+
+        long answer;
+
+        if (isAdd) {
+            answer = a + b;
+        } else {
+            answer = a - b;
+        }
+
+        return answer;
+    }
+
+    private long getAnswerFromJson(String json) {
+
+        long answer = 0;
+
         JsonReader reader = Json.createReader(new StringReader(json));
         JsonObject jsonObject = reader.readObject();
         reader.close();
 
-        // now start extracting the data you need from the json....
+        JsonArray terms = jsonObject.getJsonArray("terms");
+        int xValue = jsonObject.getInt("xValue");
 
-        // get the start value from the Json
-        int startValue = jsonObject.getInt("startValue");
+        for (int i = 0; i < terms.size(); i++) {
 
-        // read the values array from the json
-        JsonArray jsonArray = jsonObject.getJsonArray("values");
+            int power = terms
+                    .getJsonObject(i).getInt("power");
+            int multiplier = terms
+                    .getJsonObject(i).getInt("multiplier");
+            boolean isAdd = terms
+                    .getJsonObject(i).getString("action").equals("add");
 
-        // now sum the array
-        int arraySum = 0;
-
-        for (int i = 0; i < jsonArray.size(); i++)
-        {
-            arraySum += jsonArray.getInt(i);
+            answer = getAddSub(isAdd, answer
+                    , getMultiple(multiplier
+                            , getPower(power, xValue)));
         }
 
-        // calculate the answer..
-        int answer = startValue - arraySum;
+        return answer;
+    }
 
-        return Integer.toString(answer);
+    private long getAnswerFromNumeric(String data) {
+
+        long answer = 0;
+
+        String[] terms = data.substring(data.indexOf("y") + 4).split("(?=[+-])");
+        int xValue = Integer.parseInt(data.substring(4, data.indexOf(";")));
+
+        for (String term : terms) {
+
+            int power = Integer.parseInt(term.substring(term.indexOf("^") + 1));
+            int multiplier = Integer.parseInt(term.substring(1, term.indexOf(".")));
+            boolean isAdd = term.charAt(0) == '+';
+
+            answer = getAddSub(isAdd, answer
+                    , getMultiple(multiplier
+                            , getPower(power, xValue)));
+        }
+
+        return answer;
+    }
+
+    @Override
+    public String getAnswer() throws IDBSSolutionException {
+
+        long answer;
+        String jsonPrefix = "json:";
+        String numericPrefix = "numeric:";
+        String data = getDataForQuestion();
+
+        if (data.startsWith(jsonPrefix)) {
+            answer = getAnswerFromJson(data.substring(jsonPrefix.length()));
+        } else if (data.startsWith(numericPrefix)) {
+            answer = getAnswerFromNumeric(data.substring(numericPrefix.length()));
+        } else {
+            answer = getAnswerFromJson(data);
+        }
+
+        return Long.toString(answer);
     }
 
 }
